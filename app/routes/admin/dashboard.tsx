fd23a0ea5e7f360a44aa7065d1be3b7ddbf0e2d9
mmujic173 <muhamedmujic173@gmail.com>
2025-05-16 03:30:24 +0200

    Dodane rute

---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      Unde alias dicta perferendis perspiciatis ipsum accusamus sequi 
+      quis dolorum, cupiditate vitae eum soluta? Vel laborum officiis 
+      voluptatum nostrum placeat enim molestiae.</div>
+  )
+}
+
+export default Dashboard
